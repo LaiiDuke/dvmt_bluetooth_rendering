cfaec6d603d9db37bf5b4b184a5b028fa0c72626
duclv <duclv@vndgroup.vn>
2022-01-22 16:55:09 +0700

    init commit

---
+++ app/src/main/java/com/example/dvmt/MainActivity.java
@@ -0,0 +1,385 @@
+package com.example.dvmt;
+
+import static android.content.ContentValues.TAG;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.Manifest;
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.bluetooth.BluetoothServerSocket;
+import android.bluetooth.BluetoothSocket;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Entity;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
+import android.os.ParcelUuid;
+import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.github.mikephil.charting.charts.LineChart;
+import com.github.mikephil.charting.data.Entry;
+import com.github.mikephil.charting.data.LineData;
+import com.github.mikephil.charting.data.LineDataSet;
+import com.github.mikephil.charting.interfaces.datasets.ILineDataSet;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Random;
+import java.util.Set;
+import java.util.UUID;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button listen, listDevices;
+    ListView listView;
+    TextView status;
+
+    BluetoothAdapter bluetoothAdapter;
+    BluetoothDevice[] btArray;
+
+    SendReceive sendReceive;
+
+    LineChart lineChart;
+
+    int counting = 0;
+
+    ArrayList<Entry> entryList = new ArrayList<>();
+
+    static final int STATE_LISTENING = 1;
+    static final int STATE_CONNECTING=2;
+    static final int STATE_CONNECTED=3;
+    static final int STATE_CONNECTION_FAILED=4;
+    static final int STATE_MESSAGE_RECEIVED=5;
+
+    int REQUEST_ENABLE_BLUETOOTH=1;
+
+    private static final String APP_NAME = "BTChat";
+    private UUID MY_UUID;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        // Quick permission check
+        int permissionCheck = this.checkSelfPermission("Manifest.permission.ACCESS_FINE_LOCATION");
+        permissionCheck += this.checkSelfPermission("Manifest.permission.ACCESS_COARSE_LOCATION");
+        if (permissionCheck != 0) {
+
+            this.requestPermissions(new String[]{Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION}, 1001); //Any number
+        }
+        registerReceiver(bReciever, new IntentFilter(BluetoothDevice.ACTION_FOUND));
+        IntentFilter filter = new IntentFilter(
+                BluetoothAdapter.ACTION_DISCOVERY_FINISHED);
+        registerReceiver(bReciever, filter);
+        findViewByIdes();
+        bluetoothAdapter=BluetoothAdapter.getDefaultAdapter();
+
+        if(!bluetoothAdapter.isEnabled())
+        {
+            Intent enableIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+            startActivityForResult(enableIntent,REQUEST_ENABLE_BLUETOOTH);
+        }
+
+        try {
+            Method getUuidsMethod = BluetoothAdapter.class.getDeclaredMethod("getUuids", null);
+            ParcelUuid[] uuids = (ParcelUuid[]) getUuidsMethod.invoke(bluetoothAdapter, null);
+
+            if(uuids != null) {
+                MY_UUID = uuids[0].getUuid();
+                for (ParcelUuid uuid : uuids) {
+                    Log.d(TAG, "UUID: " + uuid.getUuid().toString());
+                }
+            }else{
+                Log.d(TAG, "Uuids not found, be sure to enable Bluetooth!");
+            }
+
+        } catch (NoSuchMethodException e) {
+            e.printStackTrace();
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();
+        } catch (InvocationTargetException e) {
+            e.printStackTrace();
+        }
+        LineDataSet lineDataSet = new LineDataSet(entryList, "CHART");
+        ArrayList<ILineDataSet> dataSets = new ArrayList<>();
+        dataSets.add(lineDataSet);
+        LineData data = new LineData(dataSets);
+        lineChart.setData(data);
+        lineChart.invalidate();
+
+        implementListeners();
+    }
+
+    private final BroadcastReceiver bReciever = new BroadcastReceiver() {
+        public void onReceive(Context context, Intent intent) {
+            String action = intent.getAction();
+            if (BluetoothDevice.ACTION_FOUND.equals(action)) {
+                Log.d("DEVICELIST", "Bluetooth device found\n");
+                BluetoothDevice device = intent.getParcelableExtra(BluetoothDevice.EXTRA_DEVICE);
+                // Create a new device item
+            }
+        }
+    };
+
+    private void implementListeners() {
+
+        listDevices.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Set<BluetoothDevice> bt= bluetoothAdapter.getBondedDevices();
+                String[] strings=new String[bt.size()];
+                btArray=new BluetoothDevice[bt.size()];
+                int index=0;
+
+                if( bt.size()>0)
+                {
+                    for(BluetoothDevice device : bt)
+                    {
+                        btArray[index]= device;
+                        strings[index]=device.getName();
+                        index++;
+                    }
+                    ArrayAdapter<String> arrayAdapter=new ArrayAdapter<String>(getApplicationContext(),android.R.layout.simple_list_item_1,strings);
+                    listView.setAdapter(arrayAdapter);
+                } else {
+
+                    bluetoothAdapter.startDiscovery();
+                    Toast.makeText(MainActivity.this, "Scanning Devices", Toast.LENGTH_LONG).show();
+
+
+                }
+            }
+        });
+
+        listen.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                ServerClass serverClass=new ServerClass();
+                serverClass.start();
+            }
+        });
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                ClientClass clientClass=new ClientClass(btArray[i]);
+                clientClass.start();
+
+                status.setText("Connecting");
+            }
+        });
+
+
+    }
+
+    Handler handler=new Handler(new Handler.Callback() {
+        @Override
+        public boolean handleMessage(Message msg) {
+
+            switch (msg.what)
+            {
+                case STATE_LISTENING:
+                    status.setText("Listening");
+                    break;
+                case STATE_CONNECTING:
+                    status.setText("Connecting");
+                    break;
+                case STATE_CONNECTED:
+                    status.setText("Connected");
+                    break;
+                case STATE_CONNECTION_FAILED:
+                    status.setText("Connection Failed");
+                    break;
+                case STATE_MESSAGE_RECEIVED:
+                    byte[] readBuff= (byte[]) msg.obj;
+                    String tempMsg=new String(readBuff,0,msg.arg1);
+
+                    //todo
+                    Log.d(TAG, "handleMessage: " + tempMsg);
+                    //todo
+
+                    String s;
+                    try {
+
+                        s = tempMsg;
+                        s = s.substring(s.indexOf("%") + 1, s.indexOf("$", 1));
+
+                    } catch (Exception e) {
+                        float random = new Random().nextFloat();
+                        s = "$dsdkjfhsdkj%" + random + "$";
+                        s = s.substring(s.indexOf("%") + 1, s.indexOf("$", 1));
+                    }
+                    entryList.add(new Entry(counting++, Float.parseFloat(s)));
+                    LineDataSet lineDataSet = new LineDataSet(entryList, "CHART");
+                    ArrayList<ILineDataSet> dataSets = new ArrayList<>();
+                    dataSets.add(lineDataSet);
+                    LineData data = new LineData(dataSets);
+                    lineChart.setData(data);
+                    lineChart.invalidate();
+                    break;
+            }
+            return true;
+        }
+    });
+
+    private void findViewByIdes() {
+        listen=(Button) findViewById(R.id.listen);
+        listView=(ListView) findViewById(R.id.listview);
+        status=(TextView) findViewById(R.id.status);
+        listDevices=(Button) findViewById(R.id.listDevices);
+        lineChart=(LineChart) findViewById(R.id.line_chart);
+    }
+
+    private class ServerClass extends Thread
+    {
+        private BluetoothServerSocket serverSocket;
+
+        public ServerClass(){
+            try {
+                serverSocket=bluetoothAdapter.listenUsingRfcommWithServiceRecord(APP_NAME,MY_UUID);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+        public void run()
+        {
+            BluetoothSocket socket=null;
+
+            while (socket==null)
+            {
+                try {
+                    Message message=Message.obtain();
+                    message.what=STATE_CONNECTING;
+                    handler.sendMessage(message);
+
+                    socket=serverSocket.accept();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                    Message message=Message.obtain();
+                    message.what=STATE_CONNECTION_FAILED;
+                    handler.sendMessage(message);
+                }
+
+                if(socket!=null)
+                {
+                    Message message=Message.obtain();
+                    message.what=STATE_CONNECTED;
+                    handler.sendMessage(message);
+
+                    sendReceive=new SendReceive(socket);
+                    sendReceive.start();
+
+                    break;
+                }
+            }
+        }
+    }
+
+    private class ClientClass extends Thread
+    {
+        private BluetoothDevice device;
+        private BluetoothSocket socket;
+
+        public ClientClass (BluetoothDevice device1)
+        {
+            device=device1;
+
+            try {
+                socket=device.createInsecureRfcommSocketToServiceRecord(device.getUuids()[0].getUuid());
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+        public void run()
+        {
+            try {
+                socket.connect();
+                Message message=Message.obtain();
+                message.what=STATE_CONNECTED;
+                handler.sendMessage(message);
+
+                sendReceive=new SendReceive(socket);
+                sendReceive.start();
+
+            } catch (IOException e) {
+                e.printStackTrace();
+                Message message=Message.obtain();
+                message.what=STATE_CONNECTION_FAILED;
+                handler.sendMessage(message);
+            }
+        }
+    }
+
+    private class SendReceive extends Thread
+    {
+        private final BluetoothSocket bluetoothSocket;
+        private final InputStream inputStream;
+        private final OutputStream outputStream;
+
+        public SendReceive (BluetoothSocket socket)
+        {
+            bluetoothSocket=socket;
+            InputStream tempIn=null;
+            OutputStream tempOut=null;
+
+            try {
+                tempIn=bluetoothSocket.getInputStream();
+                tempOut=bluetoothSocket.getOutputStream();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+            inputStream=tempIn;
+            outputStream=tempOut;
+        }
+
+        public void run()
+        {
+            byte[] buffer=new byte[1024];
+            int bytes;
+
+            while (true)
+            {
+                try {
+                    bytes=inputStream.read(buffer);
+                    handler.obtainMessage(STATE_MESSAGE_RECEIVED,bytes,-1,buffer).sendToTarget();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                    float random = new Random().nextFloat();
+                    buffer = ("$dsdkjfhsdkj%" + random * 100 + "$").getBytes(StandardCharsets.UTF_8);
+                    handler.obtainMessage(STATE_MESSAGE_RECEIVED,buffer.length,-1,buffer).sendToTarget();
+                }
+            }
+        }
+
+        public void write(byte[] bytes)
+        {
+            try {
+                outputStream.write(bytes);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
